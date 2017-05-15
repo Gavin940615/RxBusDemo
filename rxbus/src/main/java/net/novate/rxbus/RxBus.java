c2ea1dfdb2112bceadd7d1177f2cb96c4be042a2
Gavin <gavin940615@qq.com>
2017-05-15 23:15:46 +0800

    New Project RxBusDemo

---
+++ rxbus/src/main/java/net/novate/rxbus/RxBus.java
@@ -0,0 +1,204 @@
+package net.novate.rxbus;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+/**
+ * Created by gavin on 2017/5/15.
+ */
+
+/**
+ * 事件总线
+ */
+public class RxBus {
+
+    private static final String TAG = "RxBus";
+
+    private final Subject<Object> bus;
+    private final Map<Class<?>, Object> busMap;
+    private final Map<Integer, Object> carMap;
+
+    private RxBus() {
+        bus = PublishSubject.create().toSerialized();
+        busMap = new ConcurrentHashMap<>();
+        carMap = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * 获取单例
+     *
+     * @return 事件总线
+     */
+    public static RxBus get() {
+        return Holder.RX_BUS;
+    }
+
+    /**
+     * 发送数据
+     *
+     * @param object 数据
+     */
+    public void post(@NonNull Object object) {
+        bus.onNext(object);
+    }
+
+    /**
+     * 发送粘性数据
+     *
+     * @param object 数据
+     */
+    public void postSticky(@NonNull Object object) {
+        busMap.put(object.getClass(), object);
+        post(object);
+    }
+
+    /**
+     * 获取数据源
+     *
+     * @param type 数据类型
+     * @param <T>  数据类型
+     * @return 数据源
+     */
+    public <T> Observable<T> toObservable(@NonNull Class<T> type) {
+        return bus.ofType(type);
+    }
+
+    /**
+     * 获取粘性数据源
+     *
+     * @param type 数据类型
+     * @param <T>  数据类型
+     * @return 数据源
+     */
+    public <T> Observable<T> toObservableSticky(@NonNull final Class<T> type) {
+        return busMap.get(type) == null ?
+                toObservable(type) :
+                toObservable(type).mergeWith(Observable.create(new ObservableOnSubscribe<T>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<T> e) throws Exception {
+                        e.onNext(type.cast(busMap.get(type)));
+                        busMap.remove(type);
+                    }
+                }));
+    }
+
+    /**
+     * 发送带编号的数据
+     *
+     * @param code   编号
+     * @param object 数据
+     */
+    public void post(int code, @NonNull Object object) {
+        Car car = new Car();
+        car.code = code;
+        car.object = object;
+        bus.onNext(car);
+    }
+
+    /**
+     * 发送带编号的粘性数据
+     *
+     * @param code   编号
+     * @param object 数据
+     */
+    public void postSticky(int code, @NonNull Object object) {
+        carMap.put(code, object);
+        post(code, object);
+    }
+
+    /**
+     * 获取带编号的数据源
+     *
+     * @param code 编号
+     * @param type 数据类型
+     * @param <T>  数据类型
+     * @return 数据源
+     */
+    public <T> Observable<T> toObservable(final int code, @NonNull Class<T> type) {
+        return bus.ofType(Car.class)
+                .filter(new Predicate<Car>() {
+                    @Override
+                    public boolean test(Car car) throws Exception {
+                        return code == car.code;
+                    }
+                })
+                .map(new Function<Car, Object>() {
+                    @Override
+                    public Object apply(Car car) throws Exception {
+                        return car.object;
+                    }
+                })
+                .ofType(type);
+    }
+
+    /**
+     * 获取带编号的粘性数据源
+     *
+     * @param code 编号
+     * @param type 数据类型
+     * @param <T>  数据类型
+     * @return 数据源
+     */
+    public <T> Observable<T> toObservableSticky(final int code, @NonNull final Class<T> type) {
+        return carMap.get(code) == null ?
+                toObservable(code, type) :
+                toObservable(code, type).mergeWith(Observable.create(new ObservableOnSubscribe<T>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<T> e) throws Exception {
+                        e.onNext(type.cast(carMap.get(code)));
+                        carMap.remove(code);
+                    }
+                }));
+    }
+
+    /**
+     * 取消绑定数据源，防止内存泄漏
+     *
+     * @param disposable 数据源取绑接口
+     */
+    public void dispose(Disposable disposable) {
+        if (disposable == null) {
+            return;
+        }
+        if (!disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
+
+    /**
+     * 批量取消绑定数据源，防止内存泄漏
+     *
+     * @param disposables 数据源取绑接口列表
+     */
+    public void dispose(List<Disposable> disposables) {
+        if (disposables == null) {
+            return;
+        }
+        for (Disposable disposable : disposables) {
+            if (!disposable.isDisposed()) {
+                disposable.dispose();
+            }
+        }
+    }
+
+    private static class Car {
+        private int code;
+        private Object object;
+    }
+
+    private static class Holder {
+        private static final RxBus RX_BUS = new RxBus();
+    }
+}
